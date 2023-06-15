d9aed31c6ddd272892466024e057d4835e05047e
Adaobi Aniuchi <adamichellle@gmail.com>
2023-06-15 00:53:54 -0400

    Remove unused route in router file

--- src/router/index.js
+++ src/router/index.js
@@ -20,11 +20,6 @@ const router = createRouter({
       component: () => import('../views/PhotoBookView.vue'),
       props: route => ({ id: route.params.id, query: route.query })
     },
-    {
-      path: '/search',
-      name: 'Search',
-      component: () => import('../views/SearchView.vue')
-    },
     { path: '/:pathMatch(.*)*',
       name: 'NotFound',
       component: () => import('../views/NotFoundView.vue') },
